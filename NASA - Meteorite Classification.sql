487fc1ab1fc5146b3ff6e187d3921ecab3a2675d
uzairezra <88769014+uzairezra@users.noreply.github.com>
2023-05-03 20:33:12 +0100

    SQL Queries for Meteorite classification

---
+++ NASA - Meteorite Classification.sql
@@ -0,0 +1,187 @@
+SELECT 
+    *
+FROM
+    meteorite_landings;
+
+ALTER Table meteorite_landings
+ADD Class varchar(255)
+
+UPDATE meteorite_landings 
+SET 
+    Class = 'Chondrites'
+WHERE
+    recclass LIKE 'CI%'
+    
+UPDATE meteorite_landings 
+SET 
+    Class = 'Chondrites'
+WHERE
+    recclass LIKE 'CI%'
+
+UPDATE meteorite_landings 
+SET 
+    Class = 'Chondrites'
+WHERE
+    recclass LIKE 'CM%'
+
+UPDATE meteorite_landings 
+SET 
+    Class = 'Chondrites'
+WHERE
+    recclass LIKE 'CO%'
+
+UPDATE meteorite_landings 
+SET 
+    Class = 'Chondrites'
+WHERE
+    recclass LIKE 'CV%'
+
+UPDATE meteorite_landings 
+SET 
+    Class = 'Chondrites'
+WHERE
+    recclass LIKE 'CK%'
+
+UPDATE meteorite_landings 
+SET 
+    Class = 'Chondrites'
+WHERE
+    recclass LIKE 'CR%'
+
+UPDATE meteorite_landings 
+SET 
+    Class = 'Chondrites'
+WHERE
+    recclass LIKE 'CH%'
+
+UPDATE meteorite_landings 
+SET 
+    Class = 'Chondrites'
+WHERE
+    recclass LIKE 'CB%'
+
+UPDATE meteorite_landings 
+SET 
+    Class = 'Chondrites'
+WHERE
+    recclass LIKE 'LL%'
+
+UPDATE meteorite_landings 
+SET 
+    Class = 'Chondrites'
+WHERE
+    recclass LIKE 'H%'
+
+UPDATE meteorite_landings 
+SET 
+    Class = 'Chondrites'
+WHERE
+    recclass LIKE 'L%'
+
+UPDATE meteorite_landings 
+SET 
+    Class = 'Chondrites'
+WHERE
+    recclass LIKE 'EH%'
+
+UPDATE meteorite_landings 
+SET 
+    Class = 'Chondrites'
+WHERE
+    recclass LIKE 'EL%'
+
+UPDATE meteorite_landings 
+SET 
+    Class = 'Chondrites'
+WHERE
+    recclass LIKE 'R%'
+
+UPDATE meteorite_landings 
+SET 
+    Class = 'Achondrites'
+WHERE
+    recclass LIKE 'HED%'
+
+UPDATE meteorite_landings 
+SET 
+    Class = 'Achondrites'
+WHERE
+    recclass LIKE 'Howardite%'
+
+UPDATE meteorite_landings 
+SET 
+    Class = 'Achondrites'
+WHERE
+    recclass LIKE 'Eucrite%'
+
+UPDATE meteorite_landings 
+SET 
+    Class = 'Achondrites'
+WHERE
+    recclass LIKE 'Diogenite%'
+
+UPDATE meteorite_landings 
+SET 
+    Class = 'Achondrites'
+WHERE
+    recclass LIKE 'Angrite%'
+
+UPDATE meteorite_landings 
+SET 
+    Class = 'Achondrites'
+WHERE
+    recclass LIKE 'Aubrite%'
+
+UPDATE meteorite_landings 
+SET 
+    Class = 'Achondrites'
+WHERE
+    recclass LIKE 'Ureilite%'
+
+UPDATE meteorite_landings 
+SET 
+    Class = 'Achondrites'
+WHERE
+    recclass LIKE 'Brachinite%'
+
+UPDATE meteorite_landings 
+SET 
+    Class = 'Achondrites'
+WHERE
+    recclass LIKE 'Lunar%'
+
+UPDATE meteorite_landings 
+SET 
+    Class = 'Stony - Iron'
+WHERE
+    recclass LIKE 'Pallasites%'
+
+UPDATE meteorite_landings 
+SET 
+    Class = 'Stony - Iron'
+WHERE
+    recclass LIKE 'Mesosiderite%'
+
+UPDATE meteorite_landings 
+SET 
+    Class = 'Iron'
+WHERE
+    recclass LIKE 'IC%'
+
+UPDATE meteorite_landings 
+SET 
+    Class = 'Iron'
+WHERE
+    recclass LIKE 'IIAB%'
+
+UPDATE meteorite_landings 
+SET 
+    Class = 'Iron'
+WHERE
+    recclass LIKE 'IIC%'
+
+UPDATE meteorite_landings 
+SET 
+    Class = 'Iron'
+WHERE
+    recclass LIKE 'IID%'
